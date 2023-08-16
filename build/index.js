63ec55b4876dfc7bdfe652a41549bc501ec8c2e2
Philip Ladendorf <philip.ladendorf@gmail.com>
2023-08-16 06:20:20 +0200

    finished lection 94

--- build/index.js
+++ build/index.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
 const numbersCollection = new NumbersCollection_1.NumbersCollection([4, 1, 4, 5, 6]);
-const sorter = new Sorter_1.Sorter(numbersCollection);
+let sorter = new Sorter_1.Sorter(numbersCollection);
+sorter.sort();
+console.log(sorter.collection);
+const charactersCollection = new CharactersCollection_1.CharactersCollection('bergduWA');
+sorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
 console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([4, 1, 4, 5, 6]);
-const sorter = new Sorter(numbersCollection);
+let sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(sorter.collection);
+
+const charactersCollection = new CharactersCollection('bergduWA');
+
+sorter = new Sorter(charactersCollection);
 sorter.sort();
 console.log(sorter.collection);
